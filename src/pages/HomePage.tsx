449ee9d3d6210b8bb7e1ac43855b1e393b1e8d25
Aaron Barlow <abarlow505@gmail.com>
2025-08-15 02:50:25 -0400

    Add home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Home from "@/components/home";
+
+/**
+ * HomePage Component
+ * Simple wrapper component that renders the main Home component
+ * Used for routing purposes
+ */
+const HomePage = () => {
+  return <Home />;
+};
+
+export default HomePage;
